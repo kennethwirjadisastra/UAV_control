b9c66e1de2c5ba6ba3b187a2763d776578732ee8
Nicolas Vaillancourt <nvaillan@ucsc.edu>
2025-07-16 16:15:26 -0700

    updated vehicle class

--- classes/Vehicle.py
+++ classes/Vehicle.py
@@ -13,53 +13,57 @@ def quaternion_derivative(q, omega):
     ])
     return dq
 
-# computes the dynamics of the system given the current state, net forces, net moments, mass and intertia
-def dynamics(state, force, moment, mass, inertia):
-    p, v, q, w = state  # position, velocity, quaternion, angular velocity
-
-    dpdt = v
-    dvdt = force / mass
-    dqdt = quaternion_derivative(q, w)
-    wdot = np.linalg.inv(inertia) @ (moment - np.cross(w, inertia @ w))
-
-    return np.array([dpdt, dvdt, dqdt, wdot], dtype=object)
-
-
 # vehicle class
 class Vehicle:
     def __init__(self, position: np.ndarray = None, velocity: np.ndarray = None, 
-            quaternion: np.ndarray = None, angular_velocity: np.ndarray = None):
+            quaternion: np.ndarray = None, angular_velocity: np.ndarray = None,
+            mass: np.ndarray = None, inertia: np.ndarray = None):
         
-        self.position = np.array(position if position else [0.0, 0.0, 0.0])
-        self.velocity = np.array(velocity if velocity else [0.0, 0.0, 0.0])
-        self.quaternion = np.array(quaternion if quaternion else [0.0, 0.0, 0.0, 1.0])
-        self.angular_velocity = np.array(angular_velocity if angular_velocity else [0.0, 0.0, 0.0])
+        # state of the system
+        self.position           = np.array(position if position else [0.0, 0.0, 0.0])
+        self.velocity           = np.array(velocity if velocity else [0.0, 0.0, 0.0])
+        self.quaternion         = np.array(quaternion if quaternion else [0.0, 0.0, 0.0, 1.0])
+        self.angular_velocity   = np.array(angular_velocity if angular_velocity else [0.0, 0.0, 0.0])
+
+        # system characteristics
+        self.mass               = np.array(mass if mass else [1])
+        self.inertia            = np.array(inertia if inertia else np.eye(3))
+        self.inv_inertia        = np.linalg.inv(inertia)
+
 
     @property
     def rotation(self):
         return R.from_quat(self.quaternion)
+    
+    def get_state(self):
+        return np.array([self.position, self.velocity, self.quaternion, self.angular_velocity], dtype=object)
+    
+    def set_state(self, state):
+        self.position, self.velocity, self.quaternion, self.angular_velocity = state
+    
+    # computes the dynamics of the system given the current state, net force and net moment
+    def dynamics(self, state, force, moment):
+        p, v, q, w = state  # position, velocity, quaternion, angular velocity
+        dpdt = v
+        dvdt = force / self.mass
+        dqdt = quaternion_derivative(q, w)
+        wdot = self.inv_inertia @ (moment - np.cross(w, self.inertia @ w))
+        return np.array([dpdt, dvdt, dqdt, wdot], dtype=object)
+    
 
-    def rk4_step(self, dt):
-        state = [self.position, self.velocity, self.quaternion, self.angular_velocity]
-        f = self.force
-        m = self.moment
-        I = self.inertia
-        mass = self.mass
+    # computes the future state of the system after 1 step of rung-kutta 4 integration
+    def rk4_step(self, state, force, moment, dt):
+        def euler_step(state, dynamics, dt):
+            return [x + dxdt*dt for x, dxdt in zip(state, dynamics)]
 
-        def add_state(state, delta):
-            return [s + d for s, d in zip(state, delta)]
+        k1 = self.dynamics(state, force, moment)
+        k2 = self.dynamics(euler_step(state, k1, dt/2), force, moment)
+        k3 = self.dynamics(euler_step(state, k2, dt/2), force, moment)
+        k4 = self.dynamics(euler_step(state, k3, dt), force, moment)
 
-        k1 = dynamics(state, f, m, mass, I)
-        k2 = dynamics(add_state(state, [dt/2 * k for k in k1]), f, m, mass, I)
-        k3 = dynamics(add_state(state, [dt/2 * k for k in k2]), f, m, mass, I)
-        k4 = dynamics(add_state(state, [dt * k for k in k3]), f, m, mass, I)
-
-        new_state = [s + dt/6 * (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]) for i, s in enumerate(state)]
-
-        # Normalize quaternion
-        new_state[2] /= np.linalg.norm(new_state[2])
-
-        self.position, self.velocity, self.quaternion, self.angular_velocity = new_state
+        next_state = [s + dt/6 * (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]) for i, s in enumerate(state)]
+        next_state[2] /= np.linalg.norm(next_state[2])
+        return next_state
 
     def __repr__(self):
         return (f"Vehicle(position={self.position}, velocity={self.velocity}, "
