0d5f4c0840e9a9844134e770914896cf69028aa1
Nicolas Vaillancourt <nvaillan@ucsc.edu>
2025-07-17 15:56:02 -0700

    changes to vehicle class

--- classes/Vehicle.py
+++ classes/Vehicle.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 # necessary functions
 def quaternion_derivative(q, omega):
@@ -15,7 +16,7 @@ def quaternion_derivative(q, omega):
     return dq
 
 # vehicle class
-class Vehicle:
+class Vehicle(ABC):
     def __init__(self, position: np.ndarray = None, velocity: np.ndarray = None, 
             quaternion: np.ndarray = None, angular_velocity: np.ndarray = None,
             mass: np.ndarray = None, inertia: np.ndarray = None):
@@ -27,7 +28,7 @@ class Vehicle:
         self.angular_velocity   = np.array([0.0, 0.0, 0.0] if angular_velocity is None else angular_velocity)
 
         # system characteristics
-        self.mass               = np.array([1] if mass is None else mass)
+        self.mass               = 1.0 if mass is None else float(mass)
         self.inertia            = np.array(np.eye(3) if inertia is None else inertia)
         self.inv_inertia        = np.linalg.inv(self.inertia)
 
@@ -36,14 +37,16 @@ class Vehicle:
     def rotation(self):
         return R.from_quat(self.quaternion)
     
+
     def get_state(self):
         return np.array([self.position, self.velocity, self.quaternion, self.angular_velocity], dtype=object)
     
+
     def set_state(self, state):
         self.position, self.velocity, self.quaternion, self.angular_velocity = state
     
-    # computes the dynamics of the system given the current state, net force and net moment
-    def dynamics(self, state, force, moment):
+
+    def _compute_state_derivative(self, state, force, moment):
         p, v, q, w = state  # position, velocity, quaternion, angular velocity
         dpdt = v
         gravity_force = np.array([0,0,-9.81])
@@ -58,55 +61,31 @@ class Vehicle:
         def euler_step(state, dynamics, dt):
             return [x + dxdt*dt for x, dxdt in zip(state, dynamics)]
 
-        k1 = self.dynamics(state, force, moment)
-        k2 = self.dynamics(euler_step(state, k1, dt/2), force, moment)
-        k3 = self.dynamics(euler_step(state, k2, dt/2), force, moment)
-        k4 = self.dynamics(euler_step(state, k3, dt), force, moment)
+        k1 = self._compute_state_derivative(state, force, moment)
+        k2 = self._compute_state_derivative(euler_step(state, k1, dt/2), force, moment)
+        k3 = self._compute_state_derivative(euler_step(state, k2, dt/2), force, moment)
+        k4 = self._compute_state_derivative(euler_step(state, k3, dt), force, moment)
 
         next_state = [s + dt/6 * (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]) for i, s in enumerate(state)]
         next_state[2] /= np.linalg.norm(next_state[2])
         return next_state
     
-    def forward(self, dt: float | np.ndarray, 
-                force=None, moment=None, n_steps: int = 64):
 
-        if np.isscalar(dt):
-            t = np.linspace(0.0, n_steps*dt, num=n_steps+1, endpoint=True) # (n_steps+1,)
-            dt = dt*np.ones(n_steps)
-        else:
-            n_steps = len(dt)
-            t = np.hstack([[0.0], np.cumsum(dt)]) # (n_steps+1,)
-        
-        force = np.zeros((n_steps, 3)) if force is None else np.asarray(force)
-        moment = np.zeros((n_steps, 3)) if moment is None else np.asarray(moment)
+    # takes the vehicle in a given state and taking an action
+    # returns the resulting net forces and net moments
+    @abstractmethod
+    def compute_forces_and_moments(self, state, action) -> tuple[np.array, np.array]:
+        pass
 
-        if force.ndim == 1:
-            force = np.tile(force, (n_steps, 1)) # (n_steps, 3)
-        if moment.ndim == 1:
-            moment = np.tile(moment, (n_steps, 1)) # (n_steps, 3)
-
-        assert force.shape == (n_steps, 3), "force must be (n_steps, 3)"
-        assert moment.shape == (n_steps, 3), "moment must be (n_steps, 3)"
-
-        # 3d position, 3d velocity, 4d quaternion, 3d angular velocity
-        p = np.zeros((n_steps+1, 3))
-        v = np.zeros((n_steps+1, 3))
-        q = np.zeros((n_steps+1, 4))
-        w = np.zeros((n_steps+1, 3))
-        
-        p[0], v[0], q[0], w[0] = self.get_state()
-
-        for i in range(n_steps):
-            # update state
-            self.set_state(self.rk4_step(self.get_state(), force[i], moment[i], dt[i]))
-            p[i+1], v[i+1], q[i+1], w[i+1] = self.get_state()
-
-        return p, v, q, w, t
 
     def __repr__(self):
         return (f"Vehicle(position={self.position}, velocity={self.velocity}, "
                 f"quaternion={self.quaternion}, angular_velocity={self.angular_velocity})")
     
+
+###################################
+## ---------- testing ---------- ##
+###################################
 
 if __name__ == '__main__':
     position = np.array([0.0,0.0,1000.0])
