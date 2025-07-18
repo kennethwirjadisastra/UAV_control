fe9e5d864a99db8be0d8f72637e7e46c80c56756
kennethwirjadisastra <kennethwirjadisastra@gmail.com>
2025-07-17 17:21:51 -0700

    state update options

--- classes/Trajectory.py
+++ classes/Trajectory.py
@@ -1,9 +1,7 @@
 import torch as pt
 import numpy as np
 import matplotlib.pyplot as plt
-import Vehicle
-
-from Vehicle import Vehicle  # assuming Vehicle is an ABC
+from Vehicle import Vehicle
 
 class DummyVehicle(Vehicle):
     def compute_forces_and_moments(self, *args, **kwargs):
@@ -15,12 +13,17 @@ class Trajectory:
         self.vehicle = vehicle
 
 
-    def forward(self, actions: np.ndarray, dt: np.ndarray, moments: np.ndarray=None):
+    def forward(self, actions: np.ndarray, dt: np.ndarray, moments: np.ndarray=None,
+                update_state=True, return_all=False):
+        '''
+        Takes vehicle and propagates its state forward in time given
+        actions, moments, and dt using rk4 integration scheme
+        '''
         vehicle = self.vehicle
         N = len(actions)
         moments = np.zeros((N, 3)) if moments is None else moments
 
-        assert len(actions) == len(moments) == len(dt)
+        assert len(actions) == len(moments) == len(dt), "actions, moments, and dt must have same length"
 
         # 3d position, 3d velocity, 4d quaternion, 3d angular velocity, time
         p = np.zeros((N+1, 3))
@@ -29,14 +32,23 @@ class Trajectory:
         w = np.zeros((N+1, 3))
         t = np.hstack([[0], np.cumsum(dt, axis=0)])
 
-        p[0], v[0], q[0], w[0] = vehicle.get_state()
+        # initial state
+        state = vehicle.get_state()
+        p[0], v[0], q[0], w[0] = state
 
         for i in range(N):
+            # propagate state
+            state = vehicle.rk4_step(state, actions[i], moments[i], dt[i])
+            p[i+1], v[i+1], q[i+1], w[i+1] = state
+            
             # update state
-            vehicle.set_state(vehicle.rk4_step(vehicle.get_state(), actions[i], moments[i], dt[i]))
-            p[i+1], v[i+1], q[i+1], w[i+1] = vehicle.get_state()
+            if update_state:
+                vehicle.set_state(state)
 
-        return p, v, q, w, t
+        # returns (N+1, 14) if return_all is true, otherwise p, v, q, w, t separately
+        if return_all:
+            return np.hstack([p,v,q,w,t[:,None]])
+        return p,v,q,w,t
 
 ###################################
 ## ---------- testing ---------- ##
@@ -58,12 +70,22 @@ if __name__ == '__main__':
     actions[:,1] = np.exp(x)
     actions[:,2] = 9.81*mass*np.ones(N) # oppose gravity (should remain at constant height)
 
-    UAV = DummyVehicle(position=position, velocity=velocity, quaternion=quaternion, angular_velocity=angular_velocity,
-                  mass=mass, inertia=inertia)
+    UAV = DummyVehicle(position=position, velocity=velocity, quaternion=quaternion, 
+                       angular_velocity=angular_velocity, mass=mass, inertia=inertia)
     UAV_traj = Trajectory(UAV)
 
-    p, v, q, w, t = UAV_traj.forward(actions=actions, dt=dt)
+    print(f'State Before forward(): {np.hstack(UAV.get_state())}\n')
+    traj = UAV_traj.forward(actions=actions, dt=dt, update_state=False, return_all=True) # return full state as (N,14) tensor
+    # position, veloctiy, quaternions, angular velocity, time
+    p = traj[:,0:3]
+    v = traj[:,3:6]
+    q = traj[:,6:10]
+    w = traj[:,10:13]
+    t = traj[:,13]
+    print(f'State After forward(): {np.hstack(UAV.get_state())} (update_state=False)\n') # should be same as state before forward
+    print(f'Trajectory End: {np.hstack([p[-1], v[-1], q[-1], w[-1]])}\n') # should be different from state before forward
     
+    # plotting
     fig = plt.figure(figsize=(16,6))
     ax1 = fig.add_subplot(1, 2, 1, projection=None)
     ax2 = fig.add_subplot(1, 2, 2, projection='3d')
@@ -85,5 +107,3 @@ if __name__ == '__main__':
     ax2.grid()
     ax2.legend()
     plt.show()
-
-    print(t)
