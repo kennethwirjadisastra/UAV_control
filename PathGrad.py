e90e3cbc103ae11b230f43ddd46a9bbb5a55142b
kennethwirjadisastra <kennethwirjadisastra@gmail.com>
2025-07-30 18:04:08 -0700

    test gradients via low resolution paths

--- PathGrad.py
+++ PathGrad.py
@@ -1,7 +1,10 @@
 import torch as pt
+import matplotlib.pyplot as plt
 from classes.TargetPath import TargetPath
 from classes.Vehicle import Vehicle
 from classes.Car import Car
+from visualization.FourViewPlot import FourViewPlot
+from classes.TargetPath import TargetPath
 
 class PathGrad:
     def __init__(self, vehicle, dt_fine=0.001, device='cpu'):
@@ -21,29 +24,9 @@ class PathGrad:
 
         return X_high_res  # list of [position, velocity, quaternion, angular_velocity]
 
-    def X_low_res(self, x0, action_plan, X_high_res):
-        T = len(action_plan) * self.dt_fine
-        N = T // self.dt_coarse
-
-        action_idx = pt.linspace(0, len(X_high_res)-2, steps=N).round().long()
-        state = [s.clone() for s in x0]
-        X_low_res = [state]
-
-        fine_steps_per_coarse_step = self.dt_coarse//self.dt_fine
-        for i, idx in enumerate(action_idx):
-            action = action_plan[idx]
-            force, moment = self.vehicle.compute_forces_and_moments(state, action)
-            state = self.vehicle.rk4_step(state, force, moment, self.dt_coarse)
-            p_low, v_low, q_low, w_low = state
-            p_high, v_high, q_high, w_high = X_high_res[(i + 1) * fine_steps_per_coarse_step]
-            correction = [p_high-p_low, v_high-v_low, q_high-q_low, w_high-w_low]
-
-            X_low_res.append([s.clone()-correction[i] for i, s in enumerate(state)])
-        
-        return X_low_res
     
     def X_low_res(self, x0, action_plan, X_high_res, low_res_steps):
-        action_plan = action_plan.clone().detach().requires_grad_(True)
+        #action_plan = action_plan.clone().detach().requires_grad_(True)
         N = len(action_plan)
         T = N * self.dt_fine
         dt_coarse = T / low_res_steps
@@ -59,6 +42,7 @@ class PathGrad:
             p_high, v_high, q_high, w_high = X_high_res[(i+1) * fine_steps_per_coarse_step]
             epsilon = [p_high-p_low, v_high-v_low, q_high-q_low, w_high-w_low]
 
+            
             corrected_state = [s.clone()-epsilon[i] for i, s in enumerate(state)]
             X_low_res.append(corrected_state)
         
@@ -77,4 +61,54 @@ class PathGrad:
 
         return loss
     
+
+if __name__ == '__main__':
+    device = 'cuda' if pt.cuda.is_available() else 'cpu'
+
+    # initial state
+    position            = pt.tensor([0.0, 0.0, 0.55])
+    velocity            = pt.tensor([0, 0, 0])
+    quaternion          = pt.tensor([1.0, 0.0, 0.0, 0.0])
+    angular_velocity    = pt.tensor([0.0, 0, 0.0])
     
+    car = Car(position, velocity, quaternion, angular_velocity)
+
+    action_plan = pt.ones((2000, 2)) * pt.tensor([0.5, 0.5])[None,:]
+    action_plan.requires_grad_(True)
+    action_plan.to(device)
+    dts = 0.04 * pt.ones(2000)
+
+    
+    path = PathGrad(car, 0.004, device=action_plan.device)
+    x0 = path.vehicle.get_state()
+    optimizer = pt.optim.Adam([action_plan], lr=1e-2)
+
+    for i in range(10):
+        high_res_path = path.X_high_res(x0, action_plan)
+        p_high = pt.stack([s[0] for s in high_res_path], dim=1)
+        
+
+        low_res_path = path.X_low_res(x0, action_plan, high_res_path, low_res_steps=200)
+        p_low = pt.stack([s[0] for s in low_res_path], dim=1)
+
+        # target path
+        dts_coarse = 0.4*pt.ones(200)
+        ts = pt.cumsum(dts_coarse, dim=0)
+        wx = ts
+        wy = 5*(1-pt.cos(ts/5))
+        wz = 0.5500 * pt.ones(200)
+
+        waypoints = pt.stack([wx, wy, wz])
+        targetPath = TargetPath(waypoints)
+
+
+        loss = pt.sum((waypoints - p_low[:,1:])**2)
+        print(loss)
+        loss.backward()
+        optimizer.step()
+    print(action_plan.grad[::10])
+
+    plt.plot(waypoints[0,:], waypoints[1,:], label='Target Path')
+    plt.plot(p_high[0,:], p_high[1,:], label='High Res Path')
+    plt.plot(p_low.detach().numpy()[0,:], p_low.detach().numpy()[1,:], label='Low Res Path')
+    plt.show()
